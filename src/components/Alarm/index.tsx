818f7607b282013b5a15abdc3bc5c1188f996dd9
Karthik <automation.karthik@gmail.com>
2019-06-19 23:54:45 +0530

    added progress bar

--- src/components/Alarm/index.tsx
+++ src/components/Alarm/index.tsx
@@ -1,20 +1,42 @@
 import React from 'react'
-import { List } from 'antd';
+import { List, Progress } from 'antd';
+import { number } from 'prop-types';
+import { max, min } from 'moment';
 
 export interface alarmType{
     title: string,
     time: string
 };
 
+function calculate(time: number){
+    let r = 0;
+    let g = 100;
+    let b = 20;
+    let c = 500;
+    r = Math.min((time/c), 1) * 255;
+    g = Math.max(0, 1- (time/c)) * 255;
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
+function percentage(time: number){
+    let c = 500;
+    return Math.min((time/c), 1) * 100;
+}
+
 const AlarmList = (props: {data: alarmType[]}) => {
     return (
         <div>
             <List dataSource={props.data} itemLayout="horizontal"
                 renderItem={item => (
-                    <List.Item style={{ background: "orange", margin: '2px 3px', padding: '5px' }}>
+                    <List.Item style={{ background: calculate(Number(item.time)), margin: '2px 3px', padding: '5px' }}>
                         <List.Item.Meta
                             title={<div>{item.title}</div>}
-                            description={<div>{item.time} seconds elasped</div>}
+                            description={
+                            <div>
+                                <div>{item.time} seconds elasped</div>
+                                <Progress percent={percentage(Number(item.time))} showInfo={false} status="active" />    
+                            </div>
+                            }
                         />
                     </List.Item>
                 )}
