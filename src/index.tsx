e551805afa07f5f572fa7d50bcc68e1971cec9b5
Karthik <automation.karthik@gmail.com>
2019-08-26 19:44:05 +0530

    disable service workers

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,11 @@ import ReactDOM from "react-dom";
 import MainLayout from "./components/MainLayout/index";
 import "./styles.scss";
 
-import * as serviceWorker from "./serviceWorker";
+// import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<MainLayout />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+// serviceWorker.register();
